ac50af0b7ec8c7f1fbf12df5def4548db880537c
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2017-09-28 13:00:52 -0400

    got app to run

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Sandwitch Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkeyburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southernburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Actual Ham Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Burger of Champions', true);
