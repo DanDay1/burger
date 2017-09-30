24f0442b129322429784e6e2c9ee6affa9c6a405
Daniel Day <danielday@Daniels-MacBook-Pro.local>
2017-09-30 00:41:10 -0400

    cleaned up code and edited css

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,12 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-	// DB is JawsDB on Heroku
+
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	// DB is local on localhost
+
 	connection = mysql.createConnection({
-		port: 3306,
+		
 		host: 'localhost',
 		user: 'root',
 		password: 'password',
--- server.js
+++ server.js
@@ -7,21 +7,17 @@ console.log("port" + port);
 
 var app = express();
 
-// Serve static content for the app from the 'public' directory
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-// Set Handlebars as the view engine
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Import routes and give the server access to them
 var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
@@ -35,60 +31,3 @@ app.listen(port, function(err) {
     }
     console.log('App listening on PORT ' + port);
 })
-// app.listen(process.env.PORT || 3306);
-
-//pp.use(express.static("public"));
-
-//new code
-
-// var express = require ('express');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
-
-// var app = express();
-// var PORT = process.env.PORT;
-
-// app.use(express.static(process.cwd() + '/public'));
-
-// app.use(bodyParser.urlencoded({extended: false}));
-// // override with POST having ?_method=DELETE
-// app.use(methodOverride('_method'));
-// var exphbs = require('express-handlebars');
-// app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-// app.set('view engine', 'handlebars');
-
-// var routes = require('./controllers/burgers_controllers.js');
-// app.use('/', routes);
-
-
-// app.listen(PORT, function(){
-// 	console.log('App listening on PORT ' + PORT);
-// })
-
-// //new code
-
-// var express = require('express');
-// var methodOverride = require('method-override');
-// var bodyParser = require('body-parser');
-
-
-// var app = express();
-
-// app.use(express.static(process.cwd() + '/public'));
-
-// app.use(bodyParser.urlencoded({
-// 	extended: false
-// }));
-// // override with POST having ?_method=DELETE
-// app.use(methodOverride('_method'));
-// var exphbs = require('express-handlebars');
-// app.engine('handlebars', exphbs({
-// 	defaultLayout: 'main'
-// }));
-// app.set('view engine', 'handlebars');
-
-// var routes = require('./controllers/burgers_controller.js');
-// app.use('/', routes);
-
-
-// app.listen(process.env.PORT || 3000);
